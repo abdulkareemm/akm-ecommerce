c71ae54007d77650e102916c3f5e042eeeb7faed
abdulkareemm <eng.kreem.96@gmail.com>
2024-02-29 09:52:27 +0300

    fix reset password api

--- app/api/users/verify-password-token/route.ts
+++ app/api/users/verify-password-token/route.ts
@@ -10,26 +10,18 @@ export const POST = async (req: Request) => {
   try {
     const { token, userId } =
       (await req.json()) as ResetPasswordLinkVerifyRequest;
-    console.log(token, userId);
-    console.log("1")
     if (!isValidObjectId(userId) || !token) {
       return NextResponse.json(
         { error: "Invalid request, userId and token is required!" },
         { status: 401 }
       );
     }
-    console.log("2");
-
     const verifyToken = await PasswordResetToken.findOne({
       user: userId,
     });
-    console.log("3");
-
     if (!verifyToken) {
       return NextResponse.json({ error: "Invalid token!" }, { status: 401 });
     }
-    console.log("4");
-
     const isMatched = await verifyToken.compareToken(token);
     if (!isMatched) {
       return NextResponse.json(
@@ -37,9 +29,6 @@ export const POST = async (req: Request) => {
         { status: 401 }
       );
     }
-    console.log("5");
-
-    // await PasswordResetToken.findByIdAndDelete(verifyToken._id);
     return NextResponse.json({ verifyToken: true });
   } catch (error) {
     return NextResponse.json(
